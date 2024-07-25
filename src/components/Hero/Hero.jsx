562ad8d50ade0a4b6ce9d130dfa527e22d45b3f4
tingly <pi@piprograms.io>
2024-07-25 11:52:13 -0400

    Project section styling

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,5 @@
 import styles from "./HeroStyles.module.css";
-import heroImg from "../../assets/hero-img.png";
+import heroImg from "../../assets/tinglyAvatar1.png";
 import sun from "../../assets/sun.svg";
 import moon from "../../assets/moon.svg";
 import githubLight from "../../assets/github-light.svg";
@@ -10,37 +10,42 @@ import { useTheme } from "../../common/ThemeContext";
 function Hero() {
   const { theme, toggleTheme } = useTheme();
 
-  const themeIcon = theme === "light" ? sun : moon;
-  const githubIcon = theme === "light" ? githubLight : githubDark;
+  const themeIcon = theme === "light" ? moon : sun;
+  const githubIcon = theme === "light" ? githubDark : githubLight;
 
   return (
     <section id="hero">
-      <div className={styles.heroSection}>
+      <div className={styles.colorModeContainer}>
         <img
-          className={styles.heroImg}
           src={heroImg}
-          alt="A cartoon avatar of tingly"
+          className={styles.hero}
+          alt="Profile picture of Harris Johnsen"
         />
         <img
           className={styles.colorMode}
           src={themeIcon}
-          alt="Light/Dark theme toggle"
+          alt="Color mode icon"
           onClick={toggleTheme}
         />
       </div>
-      <div className={styles.info}>
-        <h1>Tingly</h1>
-        <h2>Software Engineer</h2>
-        <span>
-          <a href="https://github.com/TinglyTingly" target="_blank">
-            <img src={githubIcon} alt="GitHub Icon" />
+      <div className={styles.heroSection}>
+        <div className={styles.info}>
+          <h1 className="title">Tingly</h1>
+          <h2>Software Engineer</h2>
+          <span>
+            <a href="https://github.com/TinglyTingly" target="_blank">
+              <img src={githubIcon} alt="GitHub Icon" />
+            </a>
+          </span>
+          <p className="description">
+            Melding design with AI to make beautiful things.
+          </p>
+          <a href={CV}>
+            <button className={styles.button} role="button">
+              Button 53
+            </button>
           </a>
-        </span>
-        <p>Melding design with AI to make beautiful things.</p>
-        <a href={CV} target="_blank">
-          {/* Add resume page? */}
-          <button className="hover">Resume</button>
-        </a>
+        </div>
       </div>
     </section>
   );
---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,42 @@
+import styles from "./ProjectsStyles.module.css";
+import viberr from "../../assets/checkmark-dark.svg";
+import ProjectCard from "../../common/ProjectCard";
+// import freshBurger from "../../assets/fresh-burger.png";
+// import hipsster from "../../assets/hipsster.png";
+// import fitLift from "../../assets/fitlift.png";
+
+function Projects() {
+  return (
+    <section id="projects" className={styles.container}>
+      <h1 className="sectionTitle">Projects</h1>
+      <div className={styles.projectsContainer}>
+        <ProjectCard
+          src={viberr}
+          link="https://github.com/tinglytingly"
+          h3="Viberr"
+          p="Streaming App"
+        />
+        <ProjectCard
+          src={viberr}
+          link="https://github.com/tinglytingly"
+          h3="Viberr"
+          p="Streaming App"
+        />
+        <ProjectCard
+          src={viberr}
+          link="https://github.com/tinglytingly"
+          h3="Viberr"
+          p="Streaming App"
+        />
+        <ProjectCard
+          src={viberr}
+          link="https://github.com/tinglytingly"
+          h3="Viberr"
+          p="Streaming App"
+        />
+      </div>
+    </section>
+  );
+}
+
+export default Projects;

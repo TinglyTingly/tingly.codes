10183b73c0e05bafb829399cdbd7a9349728737e
tingly <pi@piprograms.io>
2024-07-26 17:59:09 -0400

    Mobile View Styling Changes

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -4,7 +4,7 @@ import watchThisSpace from "../../assets/watchThisSpace.jpg";
 function Resume() {
   return (
     <section id="resume" className={styles.container}>
-      <h1>Updated Resume</h1>
+      <h1>Resume</h1>
       <h2>Coming soon...</h2>
       <img src={watchThisSpace} alt="" className={styles.img} />
     </section>
